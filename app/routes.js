cb20434dcdc24f3058935014c893b1540700fb92
Vizards <gvizards@gmail.com>
2018-04-23 00:46:01 +0800

    init: new electron react app

---
+++ app/routes.js
@@ -0,0 +1,19 @@
+/* eslint flowtype-errors/show-errors: 0 */
+import React from 'react';
+import { Switch, Route } from 'react-router';
+import App from './containers/App';
+import HomePage from './containers/HomePage';
+import LoginPage from './containers/LoginPage';
+import RegisterPage from './containers/RegisterPage';
+import MainPage from './containers/MainPage';
+
+export default () => (
+  <App>
+    <Switch>
+      <Route path="/login" component={LoginPage} />
+      <Route path="/register" component={RegisterPage} />
+      <Route path="/main" component={MainPage} />
+      <Route path="/" exact component={HomePage} />
+    </Switch>
+  </App>
+);
